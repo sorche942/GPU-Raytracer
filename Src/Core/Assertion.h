da8e3c3a026336682d154dc3fa9b8f237eec1944
Sorche <vagol942@outlook.com>
2025-07-05 23:19:55 +0200

    removes windows specific debug assertion

--- Src/Core/Assertion.h
+++ Src/Core/Assertion.h
@@ -1,11 +1,23 @@
 #pragma once
 #include <cstdio>
+#include <cstdlib>
+
+// Platform-specific debug break
+#ifdef _WIN32
+    #include <intrin.h>
+    #define DEBUG_BREAK() __debugbreak()
+#elif defined(__GNUC__) || defined(__clang__)
+    #include <signal.h>
+    #define DEBUG_BREAK() raise(SIGTRAP)
+#else
+    #define DEBUG_BREAK() abort()
+#endif
 
 #define ASSERT(assertion)                                                          \
 	do {                                                                           \
 		if (!(assertion)) {                                                        \
 			printf("%s:%i: ASSERT(" #assertion ") failed!\n", __FILE__, __LINE__); \
-			__debugbreak();                                                        \
+			DEBUG_BREAK();                                                         \
 		}                                                                          \
 	} while(false)
 
